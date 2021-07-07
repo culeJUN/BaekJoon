f9a290d545da1f8739ca3d74469f6e099a8795ce
culeJUN <jun971103@naver.com>
2021-07-07 17:34:53 +0900

    소트인사이드

---
+++ 12.정렬/12-05.py
@@ -0,0 +1,11 @@
+import sys
+
+n = int(sys.stdin.readline())
+list_num = []
+for i in str(n) :
+    list_num.append(int(i))
+
+list_num.sort(reverse = True) 
+
+for i in list_num :
+    print(i, end = '')
