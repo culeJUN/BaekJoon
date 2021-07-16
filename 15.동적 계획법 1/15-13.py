8907d14f01b4e00279e4723c6790c7fc359338e9
culeJUN <jun971103@naver.com>
2021-07-16 16:12:22 +0900

    전깃줄

---
+++ 15.동적 계획법 1/15-13.py
@@ -0,0 +1,25 @@
+line = int(input())
+AB_line = []
+for _ in range(line):
+    A, B = map(int, input().split())
+    AB_line.append([A,B])
+
+AB_line = sorted(AB_line, key = lambda x: x[0])
+
+result = [[] for _ in range(line)]
+for i in range(line):
+    if i == 0:
+        result[i].append(AB_line[i][1])
+    else:
+        for j in range(0, i):
+            if result[j][-1] < AB_line[i][1]:
+                if len(result[i]) - 1 < len(result[j]):
+                    result[i] = result[j] + [AB_line[i][1]]
+                    
+        if not result[i]:
+            result[i].append(AB_line[i][1])
+
+maximum = 0
+for i in range(line):
+    maximum = max(maximum, len(result[i]))
+print(line - maximum)
