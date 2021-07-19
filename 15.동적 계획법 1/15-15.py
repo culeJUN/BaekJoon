198a5f5180ffd97d32edb719aac28b4c52e9b0cc
culeJUN <jun971103@naver.com>
2021-07-19 16:08:17 +0900

    연속합

---
+++ 15.동적 계획법 1/15-15.py
@@ -0,0 +1,8 @@
+x = int(input())
+num = list(map(int, input().split()))
+table = [num[0]]
+
+for i in range(x - 1) :
+    table.append(max(table[i] + num[i + 1], num[i + 1]))    #앞에 누적수에 지금 수 더한거 보다 그냥 지금 수가 더 크면 지금 수 저장
+
+print(table)
