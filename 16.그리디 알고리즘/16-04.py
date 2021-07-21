b947e3033f832ec828d670bf78b37eec690457a4
culeJUN <jun971103@naver.com>
2021-07-21 14:05:52 +0900

    잃어버린 괄호

--- 16.그리디 알고리즘/16-04.py
+++ 16.그리디 알고리즘/16-04.py
@@ -1,14 +1,14 @@
-sik = input().split('-')
+sik = input().split('-')        # 제일 큰 수를 뺴야 되므로 -기준으로 분리
 num = []
 for i in sik :
     sum = 0
-    short = i.split('+')
+    short = i.split('+')            # 분리된 식들 계산하기 위해 분리
     for j in short :
-        sum += int(j)
+        sum += int(j)                   # 더하기
     num.append(sum)
 
 n = num[0]
-for i in range(1, len(num)) :
-    n -= num[i]
+for i in range(1, len(num)) :       # 더해 놓은 것들 서로 빼주기
+    n -= num[i]                         
 
 print(n)
