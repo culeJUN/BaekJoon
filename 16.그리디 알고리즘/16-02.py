a8111966196bfaa8e0dcde1b21ff4cd7864168ae
culeJUN <jun971103@naver.com>
2021-07-21 11:14:18 +0900

    회의실 배정

--- 16.그리디 알고리즘/16-02.py
+++ 16.그리디 알고리즘/16-02.py
@@ -3,7 +3,8 @@ time = []
 for i in range(num):
     start, end = map(int, input().split())
     time.append([start, end])
-time = sorted(time, key=lambda a: a[1])     #끝나는 시간 기준 올림차순정렬
+time = sorted(time, key=lambda a: a[0])     #일단 시작 시간 기준 올림차순 정렬하고
+time = sorted(time, key=lambda a: a[1])     #끝나는 시간 기준 올림차순 정렬
 last = 0                #끝나는 시간 저장
 cnt = 0
 for i, j in time:       #타임 리스트에 저장된 시작, 종료 시간이 i랑 j에 저장됨
