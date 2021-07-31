e3fe42efe5d62da27818c87e779a8bf4f9699ae4
culeJUN <jun971103@naver.com>
2021-07-31 17:12:57 +0900

    숫자 카드 2

--- 21.이분 탐색/21-02.py
+++ 21.이분 탐색/21-02.py
@@ -1,27 +1,25 @@
-import sys
-input = sys.stdin.readline
+from sys import stdin
+n = stdin.readline()
+N = sorted(map(int,stdin.readline().split()))
+m = stdin.readline()
+M = map(int,stdin.readline().split())
 
-n = int(input())
-N = list(sorted(map(int, input().split())))
-m = int(input())
-M = list(map(int, input().split()))
-
-def binary(i, N, start, end) :
-    if start > end :
-        return 0 
-    m = (start + end) // 2
-    if i == N[m] :
-        return N[start:end + 1].count(i)
-    elif i < N[m] :
-        return binary(i, N, start, m - 1)
-    else :
-        return binary(i, N, m + 1, end)
+def binary(n, N, start, end):
+    if start > end:
+        return 0
+    m = (start+end)//2
+    if n == N[m]:
+        return N[start:end+1].count(n)
+    elif n < N[m]:
+        return binary(n, N, start, m-1)
+    else:
+        return binary(n, N, m+1, end)
 
 count = {}
-for i in N :
+for n in N:
     start = 0
     end = len(N) - 1
-    if i not in count :
-        count[i] = binary(i, N, start, end)
-        
-print(' '.join(str(count[x]) if x in count else '0' for x in M)) 
+    if n not in count:
+        count[n] = binary(n, N, start, end)
+
+print(' '.join(str(count[x]) if x in count else '0' for x in M ))
