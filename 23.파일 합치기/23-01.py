dadebfb6b3275074a541062b1afc8927a439582d
culeJUN <jun971103@naver.com>
2021-08-09 15:35:30 +0900

    파일 합치기

---
+++ 23.파일 합치기/23-01.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+for i in range(n) :
+    num = int(input())
+    file = [0] + list(map(int, input().split()))
+    sum = [0 for _ in range(num + 1)]
+    for i in range(1, num + 1) :
+        sum[i] = sum[i - 1] + file[i]
+    
+    DP = [[0 for _ in range(num + 1)] for _ in range(num + 1)]
+    for i in range(2, num + 1) :            # 2개씩 묶는거 부터 시작
+        for j in range(1, num + 2 - i) :
+            DP[j][j+i-1] = min([DP[j][j+k] + DP[j+k+1][j+i-1] for k in range(i-1)]) + (sum[j+i-1] - sum[j-1])
+    
+    print(DP[1][-1])
