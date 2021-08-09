89e1ffb20887a2142df384ac3e644112f912ac7a
culeJUN <jun971103@naver.com>
2021-08-09 12:33:12 +0900

    최대 힙

---
+++ 22.우선순위 큐/22-01.py
@@ -0,0 +1,15 @@
+import sys 
+import heapq
+input = sys.stdin.readline
+
+n = int(input())
+heap = []
+for i in range(n) :
+    command = int(input())
+    if command == 0 :
+        if len(heap) == 0 :
+            print(0)
+        else :
+            print(-1 * heapq.heappop(heap))
+    else :
+        heapq.heappush(heap, -command)
