4b342fe32e25d746e54b700e1ea7d05dd14e29e7
culeJUN <jun971103@naver.com>
2021-08-25 11:53:37 +0900

    바이러스(DFS)

---
+++ 24.DFS와 BFS/24-02-1DFS.py
@@ -0,0 +1,20 @@
+from sys import stdin
+input = stdin.readline
+dic = {}
+visited = []
+for i in range(int(input())) :
+    dic[i + 1] = set()
+
+for i in range(int(input())) :
+    a, b = map(int, input().split())
+    dic[a].add(b)
+    dic[b].add(a)
+
+def dfs(start, dic) :
+    for i in dic[start] :
+        if i not in visited :
+            visited.append(i)
+            dfs(i, dic)
+
+dfs(1, dic) 
+print(len(visited) - 1)
